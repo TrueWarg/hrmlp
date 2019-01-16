a44d5d3e41ceccd4453bcc1ceecc1e341511af7b
Irrmm <grazashar@mail.ru>
2019-01-16 21:56:38 +0200

    Add method for create some tree grid search params

--- mlmethods/decisiontrees.py
+++ mlmethods/decisiontrees.py
@@ -6,16 +6,29 @@ def get_trained_decision_tree_classifier_search_cv(
     y_train, 
     tree_grid_params, 
     cross_validation_coun=DEFALULT_CROSS_VALIDATION_COUNT, 
-    jobs_count=DEFALULT_JOBS_COUNT):
+    jobs_count=DEFALULT_JOBS_COUNT,
+    verbose=True):
 
-    tree_grid=GridSearchCV(estimator=DecisionTreeClassifier(), 
-    param_grid=tree_grid_params, 
-    cv=cross_validation_coun, 
-    n_jobs=n_jobs,
-    verbose=True)
+    tree_grid=GridSearchCV(
+        estimator=DecisionTreeClassifier(), 
+        param_grid=tree_grid_params, 
+        cv=cross_validation_coun, 
+        n_jobs=jobs_count,
+        verbose=verbose
+    )
 
     tree_grid.fit(X_train, y_train)
     return tree_grid
+
+def get_tree_classifier_grid_params(
+    max_depth_range=range(1,DEFALULT_TOP_MAX_FEATURES_VALUE),
+    top_max_features_range=(1, DEFALULT_TOP_MAX_FEATURES_VALUE)):
+
+    tree_params = {}
+    tree_params[TREE_CLS_PARAM_MAX_DEPTH] = max_depth_range
+    tree_params[TREE_CLS_MAX_FEATURES] = top_max_features_range
+
+    return tree_params
 
 # hyper params 
 TREE_CLS_PARAM_MAX_DEPTH = 'max_depth'
