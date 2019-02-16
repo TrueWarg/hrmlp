4ea00852a64db30762c78132c6cc558d83a60e1c
Irrmm <grazashar@mail.ru>
2019-02-16 14:05:33 +0200

    Add elementary functionality for save/load models

--- modelstorage/modelstorage.py
+++ modelstorage/modelstorage.py
@@ -1,6 +1,21 @@
-def save_to_storage(model):
-    return
+from joblib import dump, load
 
-def get_from_storage(model_id):
-    return
+def save_to_storage(trained_model, model_id):
+    filename = __get_file_name__(model_id)
+    dump_result = dump(trained_model, filename)
+    return dump_result
 
+def get_from_storage_by_id(model_id):
+    filename =  __get_file_name__(model_id)
+    load_result = load(filename) 
+    return load_result
+
+def get_from_storage_by_file_name(filename):
+    load_result = load(filename)
+    return load_result
+
+def __get_file_name__(variable_part):
+    return __TRAINED_MODEL_FILE_NAME_PREFIX__ + str(variable_part) + __FILE_EXTENSION__
+
+__TRAINED_MODEL_FILE_NAME_PREFIX__ = 'trained_model'
+__FILE_EXTENSION__ = '.joblib'
