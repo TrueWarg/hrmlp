3a63c7967b7cf997070c48dc98232c2230e21bc6
Irrmm <grazashar@mail.ru>
2019-02-20 20:35:39 +0200

    Add todo for bad places

--- api/static/res.py
+++ api/static/res.py
@@ -1,2 +1,2 @@
-UPLOAD_TRAINED_MODEL_SUCCESS_MESSAGE = 'Your stupid and overfitted model has loaded success'
-UPLOAD_TRAINED_MODEL_ERROR_MESSAGE = 'Upload model has failed'
+UPLOAD_TRAINED_MODEL_SUCCESS_MESSAGE = 'Your stupid and overfitted model has been loaded successfully :('
+UPLOAD_TRAINED_MODEL_ERROR_MESSAGE = 'Error while upload or train'
