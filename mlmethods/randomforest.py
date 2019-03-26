6e438532c6ae8f0a7ea8062a33ccab572171d31c
Irrmm <grazashar@mail.ru>
2019-03-26 22:37:44 +0200

    [DefaultLearning] Change default method for random forest learning

--- mlmethods/randomforest.py
+++ mlmethods/randomforest.py
@@ -3,26 +3,11 @@ from sklearn.model_selection import GridSearchCV, StratifiedKFold
 import mlmethods.constatns as const
 
 # this method for default training functionality 
-def get_trained_random_forest_classifier_search_cv(X_train, y_train):
-    stratified_k_fold = StratifiedKFold(n_splits=const.STRATIFIED_K_FOLD_COUNT, shuffle=True)
-    forest_params = {
-        const.RANDOM_FOREST_HYPER_PARAM_MAX_DEPTH : const.MAX_DEPTH_VALUES_LIST, 
-        const.RANDOM_FOREST_HYPER_PARAM_MAX_FEATURES : range(1, const.TOP_MAX_FEATURE_VALUE),
-        const.RANDOM_FOREST_HYPER_PARAM_MIN_SAMPLES_LEAF : const.MIN_SAMPLES_LEAF_VALUES_LIST
-    }
-    
-    random_forest_clf = RandomForestClassifier(
+def train_simple_random_forest(X_train, y_train):
+    forest = RandomForestClassifier(
         n_estimators=const.RANDOM_FOREST_ESTIMATORS_COUNT, 
-        n_jobs=const.JOBS_NUMBER, 
+        max_depth=const.RANDOM_FORES_TOP_MAX_DEPTH_VALUE,
+        n_jobs=const.JOBS_NUMBER,
         oob_score=const.RANDOM_FOREST_OOB_SCORE
     )
-    
-    random_forest_grid = GridSearchCV(
-        estimator=random_forest_clf, 
-        param_grid=forest_params, 
-        n_jobs=const.JOBS_NUMBER, 
-        cv=stratified_k_fold, 
-        scoring=const.GRID_SEARCH_CV_SCORING)  
-
-    random_forest_grid.fit(X_train, y_train)
-    return random_forest_grid
+    return forest
--- mlmethods/training.py
+++ mlmethods/training.py
@@ -8,13 +8,21 @@ from mlmethods.constatns import TEST_SAMPLES_SIZE
 from mlmethods.entities.metrics import ClassificationReport, ConfusionMatrix
 
 def default_random_forest(data_set, target):
-    df = pd.read_csv(data_set)
-    y = df[target]
-    X = __binarize_object_features(df, target)
-    random_forest_grid = randomforest.get_trained_random_forest_classifier_search_cv(X, y)
+    X, y = __get_X_to_y(data_set, target)
+    X_train, X_holdout, y_train, y_holdout = train_test_split(X, y, test_size=TEST_SAMPLES_SIZE)
+    forest = randomforest.train_simple_random_forest(X, y)
+    y_predicted = forest.predict(X_holdout)
     # TODO Add generation uni model id 
-    storage.save_to_storage_by_id(random_forest_grid, 'hrml_test')
-    return storage
+    model_id = 'hrml_test'
+    storage.save_to_storage_by_id(random_forest_grid, model_id)
+    report = ClassificationReport(
+        precision = precision_score(y_holdout, y_predicted),
+        recall = recall_score(y_holdout, y_predicted),
+        f1_score = f1_score(y_holdout, y_predicted),
+        accuracy = accuracy_score(y_holdout, y_predicted),
+        confusion_matrix = __convert_confusion_matrix_to_object(confusion_matrix(y_holdout, y_predicted).ravel())
+    )
+    return report
 
 def get_default_tree_with_report(data_set, target):
     X, y = __get_X_to_y(data_set, target)
@@ -23,9 +31,7 @@ def get_default_tree_with_report(data_set, target):
     y_predicted = tree_grid.predict(X_holdout)
     # TODO Add generation uni model id 
     model_id = 'hrml_test'
-    best_estimator = tree_grid.best_estimator_
-    best_estimator.fit(X, y)
-    storage.save_to_storage_by_id(best_estimator, model_id)
+    storage.save_to_storage_by_id(tree_grid, model_id)
     report = ClassificationReport(
         precision = precision_score(y_holdout, y_predicted),
         recall = recall_score(y_holdout, y_predicted),
