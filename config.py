32694434b5e0faf60202afdbcaf066a5d9cb6463
Irrmm <grazashar@mail.ru>
2019-05-11 09:18:02 +0200

    [Common] add requirements.txt

--- config.py
+++ config.py
@@ -8,3 +8,5 @@ class Config(object):
     SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or \
         'sqlite:///' + os.path.join(basedir, 'reservehrmlp.db')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
+    LOG_TO_STDOUT = os.environ.get('LOG_TO_STDOUT')
+
